63f59d6ec1a8c1999774ad70958f711b8144a8da
IraEiza <iratze.eizaguirre@gmail.com>
2024-06-19 19:16:02 +0100

    Passed first test

--- src/main.test.js
+++ src/main.test.js
@@ -1,10 +1,22 @@
-import { describe, it, expect } from "vitest"
-import { myFunction } from "./main.js"
+import { describe, expect, it } from "vitest";
+import { Item, Shop } from "./gilded_rose";
 
-describe("Default test", () => {
-  it("should work", () => {
-    const result = myFunction()
+describe("Gilded Rose", function() {
+  const names = ["Apple", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", 'Sulfuras, Hand of Ragnaros']
+  const qualities = [-1, 0, 1, 49, 50, 51]
+  const sellIns = [10, 11, 12, 5, 6, 7, -1, 0, 1, 2, 3, 4]
 
-    expect(result).toBe(true)
-  })
-})
+  
+  for(const name of names) {
+    for(const quality of qualities) {
+      for(const sellIn of sellIns) {
+        it(`Works with ${name} - ${quality} - ${sellIn}`, function() {
+          const gildedRose = new Shop([new Item(name, sellIn, quality)]);
+          const items = gildedRose.updateQuality();
+          expect(items[0]).toMatchSnapshot();
+        });
+      }
+    }
+  }
+});
+
